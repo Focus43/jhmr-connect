e2c57561f0574ae7c0e033a81b59b193820e4cbd
Stine Richvoldsen <superrunt@gmail.com>
2014-10-20 10:18:39 -0600

    loading modal, participant list

--- source/js/participant/controllers/participant.js
+++ source/js/participant/controllers/participant.js
@@ -1,4 +1,10 @@
 angular.module('jhmrc.participant').
     controller('ParticipantCtrl', ['$scope', '$http', '$location', '$rootScope', 'Participant', function( $scope, $http, $location, $rootScope, Participant ){
 
+        $scope.fullName = $scope.participant.ParticipantFirstName + " " + $scope.participant.ParticipantLastName;
+
+        $scope.goToReportCard = function (cardId) {
+            $location.path("/reportcard/" + cardId);
+        }
+
     }]);
