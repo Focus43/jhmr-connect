d07e6bf933404bcaf03841bfb4ea0b972a9c7276
Stine Richvoldsen <superrunt@gmail.com>
2014-10-22 09:24:00 -0600

    report card update: lift icons

--- source/js/reportcard/controllers/reportcard.js
+++ source/js/reportcard/controllers/reportcard.js
@@ -2,15 +2,30 @@ angular.module('jhmrc.reportcard').
     controller('ReportCardCtrl', ['$scope', '$http', '$location', '$rootScope', 'ReportCard', function( $scope, $http, $location, $rootScope, ReportCard ){
 
         $scope.reportcard = {
-            ParticipantFirstName: "Charlie",
-            ParticipantLastName: "Hagen",
+            participantFirstName: "Charlie",
+            participantLastName: "Hagen",
+
             date: "03/05/2014",
             classDescr: "6-9 yr old",
-            level: "6",
-            lifts: [
-                { liftname: "Tram", vert: "800" },
-                { liftname: "Gondola", vert: "500" }
+
+            level: {
+                discipline: "Alpine",
+                rank: "2",
+                shortName: "Level 2"
+            },
+
+            terrain: [
+                { name: "Bumps", acronym: "BMP" },
+                { name: "Chutes", acronym: "CHU" },
+                { name: "Powder", acronym: "POW" }
             ],
+
+            lifts: [
+                { liftId: "bc0a1e78-2284-4d67-aa2e-2f8448ae341c", acronym: "TRAM", name: "Tram", elevationGain: "4800" },
+                { liftId: "bc0a1e78-2284-4d67-aa2e-2f8448ae341c", acronym: "SUBL", name: "Sublette", elevationGain: "800" },
+                { liftId: "bc0a1e78-2284-4d67-aa2e-2f8448ae341c", acronym: "GONDY", name: "Gondola", elevationGain: "500" }
+            ],
+
             skills: [
                 { skill: "turn", completed: "4" },
                 { skill: "hucking", completed: "2" },
